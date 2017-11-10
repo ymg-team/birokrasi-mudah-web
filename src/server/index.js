eb136d597d1c45c01bfffb44b2a4e90dad37228b
yussan <yussandeveloper@gmail.com>
2017-11-10 10:13:32 +0700

    set port to 5000

--- src/server/index.js
+++ src/server/index.js
@@ -2,7 +2,7 @@ import Express from 'express'
 import Render from './render'
 import Api from './api'
 import Path from 'path'
-const PORT = process.env.PORT || 8080
+const PORT = process.env.PORT || 5000
 const App = Express()
 
 App.use('/bundle', Express.static(Path.resolve(__dirname + '/../../bundle')))
