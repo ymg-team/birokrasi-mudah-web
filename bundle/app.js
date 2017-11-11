ebfcef88642a280c4189c47aec9657c8df2cfd28
yussan <yussandeveloper@gmail.com>
2017-11-11 20:12:10 +0700

    maping

--- bundle/app.js
+++ bundle/app.js
@@ -15326,6 +15326,8 @@ Object.defineProperty(exports, "__esModule", {
     value: true
 });
 
+var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
+
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
 var _react = __webpack_require__(0);
@@ -15354,77 +15356,22 @@ var CardMap = function (_Component) {
         value: function render() {
             var location = this.props.location;
 
-            console.log(location);
-            var firstLoc = location[0];
+            console.log(location[0]);
             return _react2.default.createElement(
                 'div',
                 { className: 'grid-center' },
-                _react2.default.createElement(
+                _react2.default.createElement(Card, _extends({ show_more: location.length > 1 }, location[0])),
+                location.length > 1 ? _react2.default.createElement(
                     'div',
-                    { className: 'col-8_md-12_sm-12' },
+                    { id: 'modal-maps', className: 'hide' },
                     _react2.default.createElement(
                         'div',
-                        { className: 'big-result-card big-result-card--map' },
-                        _react2.default.createElement(
-                            'div',
-                            { className: 'big-result-card-map-embed' },
-                            _react2.default.createElement('iframe', { className: 'map', src: '//www.google.com/maps/embed/v1/place?q=samsat,bekasi&zoom=17&key=AIzaSyALC1j4_h9NQGFcK3wtYHVDxn8fFhwB210' })
-                        ),
-                        _react2.default.createElement(
-                            'div',
-                            { className: 'big-result-card-map-text' },
-                            _react2.default.createElement(
-                                'p',
-                                null,
-                                ' ',
-                                _react2.default.createElement(
-                                    'strong',
-                                    null,
-                                    'Kantor Samsat Bekasi'
-                                ),
-                                _react2.default.createElement('br', null),
-                                'Jl. Maju Negara 20, Bekasi Selatan '
-                            ),
-                            _react2.default.createElement('hr', null),
-                            _react2.default.createElement(
-                                'p',
-                                null,
-                                _react2.default.createElement(
-                                    'strong',
-                                    null,
-                                    'Jadwal ',
-                                    _react2.default.createElement(
-                                        'span',
-                                        { className: 'text-red' },
-                                        '(hari ini tutup)'
-                                    )
-                                )
-                            ),
-                            _react2.default.createElement(
-                                'p',
-                                null,
-                                'Senin - Jum\'at :\xA0 ',
-                                _react2.default.createElement(
-                                    'strong',
-                                    null,
-                                    '07:00 - 14:00 WIB '
-                                ),
-                                _react2.default.createElement('br', null),
-                                'Sabtu :\xA0',
-                                _react2.default.createElement(
-                                    'strong',
-                                    null,
-                                    '07:00 - 15:00 WIB '
-                                )
-                            ),
-                            location.length > 1 ? _react2.default.createElement(
-                                'a',
-                                { className: 'btn btn-outline-white', href: 'javascript:;', style: { display: 'block', textDecoration: 'none', textAlign: 'center' } },
-                                'Tampilan lokasi lainnya'
-                            ) : null
-                        )
+                        { className: 'grid-center' },
+                        location.map(function (n, key) {
+                            return _react2.default.createElement(Card, _extends({ key: key }, n));
+                        })
                     )
-                )
+                ) : null
             );
         }
     }]);
@@ -15433,6 +15380,81 @@ var CardMap = function (_Component) {
 }(_react.Component);
 
 exports.default = CardMap;
+
+
+var Card = function Card(props) {
+    return _react2.default.createElement(
+        'div',
+        { className: 'col-8_md-12_sm-12' },
+        _react2.default.createElement(
+            'div',
+            { className: 'big-result-card big-result-card--map' },
+            _react2.default.createElement(
+                'div',
+                { className: 'big-result-card-map-embed' },
+                _react2.default.createElement('iframe', { className: 'map', src: '//www.google.com/maps/embed/v1/place?q=' + props.coordinat.lat + ',' + props.coordinat.lng + '&zoom=17&key=AIzaSyALC1j4_h9NQGFcK3wtYHVDxn8fFhwB210' })
+            ),
+            _react2.default.createElement(
+                'div',
+                { className: 'big-result-card-map-text' },
+                _react2.default.createElement(
+                    'p',
+                    null,
+                    _react2.default.createElement(
+                        'strong',
+                        null,
+                        props.name
+                    ),
+                    _react2.default.createElement('br', null),
+                    props.desc
+                ),
+                _react2.default.createElement('hr', null),
+                _react2.default.createElement(
+                    'p',
+                    null,
+                    _react2.default.createElement(
+                        'strong',
+                        null,
+                        'Jadwal ',
+                        _react2.default.createElement(
+                            'span',
+                            { className: 'text-red' },
+                            '(hari ini tutup)'
+                        )
+                    )
+                ),
+                _react2.default.createElement(
+                    'p',
+                    null,
+                    'Senin - Jum\'at :\xA0 ',
+                    _react2.default.createElement(
+                        'strong',
+                        null,
+                        '07:00 - 14:00 WIB '
+                    ),
+                    _react2.default.createElement('br', null),
+                    'Sabtu :\xA0',
+                    _react2.default.createElement(
+                        'strong',
+                        null,
+                        '07:00 - 15:00 WIB '
+                    )
+                ),
+                props.show_more ? _react2.default.createElement(
+                    'a',
+                    {
+                        className: 'btn btn-outline-white',
+                        href: 'javascript:;',
+                        onClick: function onClick() {
+                            modal.show('Lokasi Tersedia', 'modal-maps');
+                        },
+                        style: { display: 'block', textDecoration: 'none', textAlign: 'center' } },
+                    'Tampilan lokasi lainnya'
+                ) : null
+            )
+        )
+    );
+};
 
 /***/ }),
 /* 124 */
--- src/client/components/atoms/BigResult/CardMap.jsx
+++ src/client/components/atoms/BigResult/CardMap.jsx
@@ -5,30 +5,55 @@ export default class CardMap extends Component
     render()
     {
         const {location} = this.props
-        console.log(location)
-        const firstLoc = location[0]
+        console.log(location[0])
         return(
             <div className='grid-center'> 
-                <div className='col-8_md-12_sm-12'>
-                    <div className='big-result-card big-result-card--map'>
-                    <div className='big-result-card-map-embed'>
-                        <iframe className='map' src='//www.google.com/maps/embed/v1/place?q=samsat,bekasi&zoom=17&key=AIzaSyALC1j4_h9NQGFcK3wtYHVDxn8fFhwB210' />
-                    </div>
-                    <div className='big-result-card-map-text'> 
-                        <p> <strong>Kantor Samsat Bekasi</strong><br />Jl. Maju Negara 20, Bekasi Selatan </p>
-                        <hr />
-                        <p><strong>Jadwal <span className='text-red'>(hari ini tutup)</span></strong></p>
-                        <p>
-                        Senin - Jum'at :&nbsp; <strong>07:00 - 14:00 WIB </strong><br />Sabtu :&nbsp;<strong>07:00 - 15:00 WIB </strong></p>
-                        {
-                            location.length > 1 ? 
-                                <a className='btn btn-outline-white' href='javascript:;' style={{display: 'block', textDecoration: 'none', textAlign: 'center'}}>Tampilan lokasi lainnya</a>
-                            : null
-                        }
-                    </div>
-                    </div>
-                </div>
+                <Card show_more={location.length > 1} {...location[0]} />
+                {/* maps modal */}
+                {
+                    location.length > 1 ? 
+                        <div id='modal-maps' className='hide'>
+                            <div className='grid-center'>
+                                {location.map((n, key) => (
+                                    <Card key={key} {...n} />
+                                ))}
+                            </div>
+                        </div>
+                    : null
+                }
             </div>
         )
     }
 }
+
+const Card = (props) => (
+    <div className='col-8_md-12_sm-12'>
+        <div className='big-result-card big-result-card--map'>
+            <div className='big-result-card-map-embed'>
+                <iframe className='map' src={`//www.google.com/maps/embed/v1/place?q=${props.coordinat.lat},${props.coordinat.lng}&zoom=17&key=AIzaSyALC1j4_h9NQGFcK3wtYHVDxn8fFhwB210`} />
+            </div>
+            <div className='big-result-card-map-text'> 
+                <p>
+                    <strong>{props.name}</strong>
+                    <br />
+                    {props.desc}
+                </p>
+                <hr />
+                <p><strong>Jadwal <span className='text-red'>(hari ini tutup)</span></strong></p>
+                <p>
+                Senin - Jum'at :&nbsp; <strong>07:00 - 14:00 WIB </strong><br />Sabtu :&nbsp;<strong>07:00 - 15:00 WIB </strong></p>
+                {
+                    props.show_more? 
+                        <a 
+                            className='btn btn-outline-white' 
+                            href='javascript:;' 
+                            onClick={() => {modal.show('Lokasi Tersedia', 'modal-maps')}} 
+                            style={{display: 'block', textDecoration: 'none', textAlign: 'center'}}>
+                            Tampilan lokasi lainnya
+                        </a>
+                    : null
+                }
+            </div>
+        </div>
+    </div>
+)
